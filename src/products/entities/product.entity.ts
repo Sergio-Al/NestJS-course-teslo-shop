28dc0b12917b1a9a45b4eb056216cf7993422f0b
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-01-01 12:17:03 -0400

    Feat. Creating fields in an Entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -9,4 +9,34 @@ export class Product {
     unique: true,
   })
   title: string;
+
+  @Column('numeric', { default: 0 })
+  price: number;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  slug: string;
+
+  @Column('int', {
+    default: 0,
+  })
+  stock: number;
+
+  @Column('text', {
+    array: true,
+  })
+  sizes: string[];
+
+  @Column('text')
+  gender: string;
+
+  // tags
+  // images
 }
