76e6ed56a57c249ab3adc691b3398d7889a03591
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-01-14 00:38:42 -0400

    Feat. Configuring JWT/Passport
    - We configured jwt with passport in our app. It's the first step the next commit enhance this feature.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,11 +4,22 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: {
+    //     expiresIn: '2h',
+    //   },
+    // }),
+  ],
   exports: [TypeOrmModule],
 })
 export class AuthModule {}
