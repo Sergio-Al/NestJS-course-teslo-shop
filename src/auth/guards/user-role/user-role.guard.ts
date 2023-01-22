af059e2011ea369b13e7256219e597a5b0a910f6
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-01-22 11:12:50 -0400

    Feat. Verifying user role
    We modified our guard, now we can check if the user have a valid role otherwise we throw an error.

--- src/auth/guards/user-role/user-role.guard.ts
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -1,5 +1,12 @@
+import { User } from './../../entities/user.entity';
 import { Reflector } from '@nestjs/core';
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  BadRequestException,
+  ForbiddenException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -12,7 +19,21 @@ export class UserRoleGuard implements CanActivate {
       'roles',
       context.getHandler(),
     );
-    console.log({ validRoles });
-    return true;
+
+    if (!validRoles) return true;
+    if (validRoles.length === 0) return true;
+
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as User;
+
+    if (!user) throw new BadRequestException('User not found');
+
+    for (const role of user.roles) {
+      if (validRoles.includes(role)) return true;
+    }
+
+    throw new ForbiddenException(
+      `User: ${user.fullName} needs a valid Role: [${validRoles}]`,
+    );
   }
 }
