ad232aded8b17fc2ce56989149a198e68a65f7fe
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-01-22 18:49:14 -0400

    Feat. Adding Swagger OpenAPI specification
    We added swagger to our app, now our endpoints are documented.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,6 +15,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Teslo Shop RESTful API')
+    .setDescription('The Teslo shop API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
   logger.log(`App running on port ${process.env.PORT}`);
 }
