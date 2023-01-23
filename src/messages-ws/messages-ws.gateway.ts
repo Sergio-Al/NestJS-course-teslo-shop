97e72cb3447652982c6ee4d42e2992d27e976c07
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-01-23 11:46:12 -0400

    Feat. Send connected clients
    We created a custom event with a payload that send to the client the list of the connected clients and updates if a client is disconnected

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -2,28 +2,34 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
+  @WebSocketServer() wws: Server; // this is the global socket instance (all clients can hear this)
+
   constructor(private readonly messagesWsService: MessagesWsService) {}
   handleConnection(client: Socket) {
     // console.log('client connected', client.id);
     this.messagesWsService.registerClient(client);
 
-    console.log({
-      connectedClients: this.messagesWsService.getConnectedClient(),
-    });
+    this.wws.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
   }
   handleDisconnect(client: Socket) {
     // console.log('client disconnected', client.id);
     this.messagesWsService.removeClient(client.id);
-    console.log({
-      connectedClients: this.messagesWsService.getConnectedClient(),
-    });
+
+    this.wws.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
   }
 }
--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -17,7 +17,7 @@ export class MessagesWsService {
     delete this.connectedClients[clientId];
   }
 
-  getConnectedClient(): number {
-    return Object.keys(this.connectedClients).length;
+  getConnectedClients(): string[] {
+    return Object.keys(this.connectedClients);
   }
 }
